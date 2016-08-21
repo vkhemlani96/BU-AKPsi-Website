02066f1920c5d5e4f514c5513ab7d7bf760ba778
vkhemlani96 <vkhemlani96@gmail.com>
2016-08-21 14:13:09 -0400

    Eye2Eye Index Updates

--- eye2eye/index.php
+++ eye2eye/index.php
@@ -28,10 +28,19 @@
 		<div class="center vertical_padding">
 
 			<p>
-				<img src="../img/eye2eye_logo.png" style="float:right; padding-left:20px" height="100">
+				<img src="../img/eye2eye_logo.png" style="float:right; padding-left:20px; padding-bottom: 20px;" height="100">
 				Eye2Eye is a student-run organization founded by Nu Chapter in 2016. The group aims to raise awareness about gender inequality in the professional world by holding a national conference with pivotal guest speakers and in collaboration with other nonprofits.
 				<br><br>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis leo non lorem blandit, sed consectetur mi porttitor. Quisque dui nulla, pretium vel ultrices ut, dapibus ut eros. Nullam molestie, enim imperdiet vestibulum rhoncus, augue nisi consequat nisl, a viverra nisl eros eget mi. Sed efficitur et magna at cursus. Praesent iaculis suscipit risus nec tempus. Ut fringilla arcu id mattis interdum. Nullam id tortor ligula. Sed aliquam commodo elit at condimentum. Proin eu turpis posuere tortor rutrum consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas volutpat ornare justo sed tempor. Curabitur in neque ac arcu rutrum porta et non nisl. Integer sed posuere mi. Vestibulum orci quam, porta eget orci id, pellentesque sagittis enim. Praesent pretium magna leo, sed hendrerit enim pellentesque ut. Donec vestibulum vestibulum tortor non semper.
+				The inspiration to start Eye2Eye started in the spring of 2016 with a few students from Boston University. Having experienced and witnessed gender inequality first hand within the business school at BU and throughout multiple industries pushed the team to create Eye2Eye.  The 3% Movement’s <a href="http://www.thedrum.com/opinion/2016/08/11/thousand-words-worth-picture-women-share-how-sexism-industry-has-affected-them">“Elephant on Madison Avenue”</a> survey was able to collect over 500 stories of gender inequality in the workplace and broke down some of the issues into categories like Sexual Harassment/Toxic Culture, Pay Inequity, Lack of Opportunity/Visibility, and The Motherhood Penalty. Eye2Eye hopes to focus and raise awareness on these topics.  
+				<br><br>
+				According to <a href="http://www.ilo.org/washington/areas/gender-equality-in-the-workplace/WCMS_159496/lang--en/index.htm">public information collected by the International Trade Union Confederation (ITUC)</a>, “the global gender pay gap ranges from 3 percent to 51 percent with a global average of 17 percent.” As noted in <a href="http://www.huffingtonpost.com/faith-popcorn/you-cant-see-the-future-f_b_1463114.html">You Can’t See the Future From the Man-Cave</a> by Faith Popcorn of the Huffington post, “A study by Catalyst found that those Fortune 500 companies with the most female directors outperformed those with few, or none. And those with at least three women at the boardroom table dramatically outperformed the rest. And the numbers were huge: 84 percent return on sales; 60 greater percent return on invested capital, and 46 percent greater return on equity.” 
+				<br><br>
+				The research continues and the issue of gender inequality is undeniable; something needs to happen. Eye2Eye is fully committed to continue collecting research and help raise awareness on the issue. With our annual conference, we aim to invite professionals from all industries from over the country to talk, discuss, and highlight some of the issues they have faced and noticed as well as how they work to combat this epidemic. Besides the conference, our team will be working hard to research different industries and gender inequality specific to Boston University. If our research is informational and unique, we hope to expand to other universities and help lead the gender inequality research platform among colleges throughout the United States.
+				<br><br>
+				Eye2Eye wants to see a fairer, more equal and just workforce, where hard work, talent, and determination are the main measures of success. In order to achieve our milestones, we need your help. With your support, we can develop a platform that fosters change and raises the awareness that has been mute in many communities. Please help us spread our mission by sharing our website and spreading our name. Additionally, please consider donating to our fund to help us create a meaningful and impactful conference; every dollar helps tremendously. We thank you for your support!
+			</p>
+			<p style="text-align:center">
+				<a target="_blank" href="https://crowdfunding.bu.edu/project/2626/donate" class="button">Give Now</a>
 			</p>
 
 		</div>
@@ -44,17 +53,17 @@
 			</div>
 		</div>
 
-		<div class="vertical_padding center">
-			<h2><strong>Help us with the Fight</strong></h2>
-			<div class="seperator"></div>
+		<!--
+<div class="vertical_padding center">
+<h2><strong>Help us with the Fight</strong></h2>
+<div class="seperator"></div>
 
-			<p class="vertical_padding">
-				In order to hold a successful conference, Eye2Eye hopes to raise a significant amount of money. If you would like to support this cause and help us raise awareness in the fight to close the gender gap, click and share the link on the right where you can make your tax-deductible donation. Every bit helps - no matter how big or small.  
-			</p>
+<p class="vertical_padding">
+In order to hold a successful conference, Eye2Eye hopes to raise a significant amount of money. If you would like to support this cause and help us raise awareness in the fight to close the gender gap, click and share the link on the right where you can make your tax-deductible donation. Every bit helps - no matter how big or small.  
+</p>
 
-			<p style="text-align:center">
-				<a target="_blank" href="https://crowdfunding.bu.edu/project/2626/donate" class="button">Give Now</a>
-			</p>
-		</div>
+</div>
+-->
+		<?php getFooter(); ?>
 	</body>
 </html>
