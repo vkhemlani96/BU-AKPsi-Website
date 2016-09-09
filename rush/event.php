0e9721b752a6a29fc788c4f1316d6ace664b1721
vkhemlani96 <vkhemlani96@gmail.com>
2016-09-09 15:44:57 -0400

    Updated Rush Events, Made Event Sign Ins/Rush Page Titles Dynamic

--- rush/event.php
+++ rush/event.php
@@ -17,7 +17,7 @@ if (isset($_POST["rushFirstName"]) && isset($_POST["rushLastName"]) && isset($_P
 	VALUES ('Event', '" . $_POST["rushFirstName"] . "', '" . $_POST["rushLastName"] . "', '" . $_POST["rushEmail"] . "', '" . $_POST["rushPhone"] . "', '" . $_POST["rushMajors"] . "', '" . $_POST["rushSchool"] . "', '" . $_POST["rushGrade"] . "', '1') ON DUPLICATE KEY UPDATE " . $_GET["event"] . "=VALUES(".$_GET['event'].")";
 
 	if ($conn->query($sql) === TRUE) {
-//		echo "New record created successfully";
+		//		echo "New record created successfully";
 	} else {
 		echo "Error: " . $sql . "<br>" . $conn->error;
 		if (strpos($conn->error,'Duplicate') !== false) {
@@ -28,37 +28,53 @@ if (isset($_POST["rushFirstName"]) && isset($_POST["rushLastName"]) && isset($_P
 	$conn->close();
 }
 
-//if (!isset($_GET["event"]) || $_GET["event"] == "" ) {
-//			die('NO Event PROVIDED');
-//}
-
 $event = $_GET["event"];
 $title = "";
 
-switch ($event) {
-	case "Info1":
-		$title = "Infosession 1";
-		break;
-	case "Info2":
-		$title = "Infosession 2";
-		break;
-	case "Fashion":
-		$title = "Fashion Night";
-		break;
-	case "Professional":
-		$title = "Professional Workshops";
-		break;
-	case "Trivia":
-		$title = "Trivia Night";
-		break;
-	case "Mocktail":
-		$title = "Mocktail Night";
-		break;
-	default:
-		die('NO Event PROVIDED
-		<p><a href="http://www.buakpsi.com/rush/event.php?event=Trivia">Trivia Night</a></p>
-		<p><a href="http://www.buakpsi.com/rush/event.php?event=Mocktail">Mocktail Night</a></p>');
+$eventList = [
+	"Info1" => "Infosession 1",
+	"Info2" => "Infosession 2",
+	"Coffeehouse" => "Coffeehouse",
+	"Professional" => "Professional Night",
+	"Fashion" => "Fashion Night",
+	"Interview" => "Interview Workshops",
+	"Mocktail" => "Mocktail Night",
+];
+
+
+if (array_key_exists($event, $eventList)) {
+	$title = $eventList[$event];
+} else {
+	$eventUrls = "";
+	$arrayKeys = array_keys($eventList);
+
+	for ($i = 0; $i < count($arrayKeys); $i++) {
+		$eventUrls .= '<p><a href="http://www.buakpsi.com/rush/event.php?event=' . $arrayKeys[$i] . '">' . $eventList[$arrayKeys[$i]] . '</a></p>';
+	}
+	die('Choose an event<br>' . $eventUrls);
 }
+
+//switch ($event) {
+//	case "Info1":
+//		$title = ;
+//		break;
+//	case "Info2":
+//		$title = "Infosession 2";
+//		break;
+//	case "Coffeehouse":
+//		$title = "Coffeehouse";
+//		break;
+//	case "Professional":
+//		$title = "Professional Night";
+//		break;
+//	case "Fashion":
+//		$title = "Fashion Night";
+//		break;
+//	case "Interview":
+//		$title = "Interview Workshops";
+//		break;
+//	default:
+//}
 
 ?>
 
@@ -66,112 +82,112 @@ switch ($event) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<title>Event Sign In | Alpha Kappa Psi Nu Chapter</title>
-	
-	<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.indigo-pink.min.css">
-	<script src="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.js"></script>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	
-	<link href="../css/styles.css" rel="stylesheet"/>
-	<link href="../css/navbar.css" rel="stylesheet" />
-	<script src="../js/jquery.js"></script>
-	<script src="../js/jquery.color.js"></script>
-</head>
+	<head>
+		<title>Event Sign In | Alpha Kappa Psi Nu Chapter</title>
 
-<body>
-	<?php include("../navbar.php"); getNavbar(true); ?>
-	
-	<div class="center vertical_padding title_section">
-		<h1>Recruitment Sign-Up</h1>
-		<div class="seperator"></div>
-		<h2><?php echo $title ?></h2>
-	</div>
-	
-	<div class="vertical_padding center">
-		<form style="text-align:center; width: 60%; margin: 0 auto;" action="event.php?event=<?php echo $event; ?>" id="rushForm" method="post">
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushEmail" name="rushEmail"/>
-				<label class="mdl-textfield__label" for="sample1">E-Mail (MUST USE @bu.edu)</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushFirstName" name="rushFirstName" />
-				<label class="mdl-textfield__label" for="sample1">First Name</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushLastName" name="rushLastName" />
-				<label class="mdl-textfield__label" for="sample1">Last Name</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushPhone" name="rushPhone"/>
-				<label class="mdl-textfield__label" for="sample1">Phone Number</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushGrade" name="rushGrade"/>
-				<label class="mdl-textfield__label" for="sample1">Grade (ex. Freshman, Sophomore...)</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushSchool" name="rushSchool"/>
-				<label class="mdl-textfield__label" for="sample1">School(s) (ex. QUESTROM, ENG)</label>
-			</div>
-			<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
-				<input class="mdl-textfield__input" type="text" id="rushMajors" name="rushMajors"/>
-				<label class="mdl-textfield__label" for="sample1">Major(s) / Concentrations(s)</label>
-			</div>
-			<br>
-			<button class="button" type="button" id="formSubmit" name="formSubmit">Sign In</button>
-		</form>
-		
-<!--		<a href="https://recruitmentpro-app.chapterspot.com/prospects/view" target="_blank"><p>Visit Recruitment Pro!</p></a>-->
-		
-	</div>
-	
-	
-	<?php getFooter(); ?>
-	
-</body>
+		<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.indigo-pink.min.css">
+		<script src="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-<script>
-		
-	function checkform() {
-		// get all the inputs within the submitted form
-		var inputs = document.getElementById("rushForm").getElementsByTagName('input');
-		for (var i = 0; i < inputs.length; i++) {
-			// only validate the inputs that have the required attribute
-			if(inputs[i].value == ""){
-				// found an empty field that is required
-				return false;
+		<link href="../css/styles.css" rel="stylesheet"/>
+		<link href="../css/navbar.css" rel="stylesheet" />
+		<script src="../js/jquery.js"></script>
+		<script src="../js/jquery.color.js"></script>
+	</head>
+
+	<body>
+		<?php include("../navbar.php"); getNavbar(true); ?>
+
+		<div class="center vertical_padding title_section">
+			<h1>Event Sign In</h1>
+			<div class="seperator"></div>
+			<h2><?php echo $title ?></h2>
+		</div>
+
+		<div class="vertical_padding center">
+			<form style="text-align:center; width: 60%; margin: 0 auto;" action="event.php?event=<?php echo $event; ?>" id="rushForm" method="post">
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushEmail" name="rushEmail"/>
+					<label class="mdl-textfield__label" for="sample1">E-Mail (MUST USE @bu.edu)</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushFirstName" name="rushFirstName" />
+					<label class="mdl-textfield__label" for="sample1">First Name</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushLastName" name="rushLastName" />
+					<label class="mdl-textfield__label" for="sample1">Last Name</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushPhone" name="rushPhone"/>
+					<label class="mdl-textfield__label" for="sample1">Phone Number</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushGrade" name="rushGrade"/>
+					<label class="mdl-textfield__label" for="sample1">Grade (ex. Freshman, Sophomore...)</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushSchool" name="rushSchool"/>
+					<label class="mdl-textfield__label" for="sample1">School(s) (ex. QUESTROM, ENG)</label>
+				</div>
+				<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label textfield-demo">
+					<input class="mdl-textfield__input" type="text" id="rushMajors" name="rushMajors"/>
+					<label class="mdl-textfield__label" for="sample1">Major(s) / Concentrations(s)</label>
+				</div>
+				<br>
+				<button class="button" type="button" id="formSubmit" name="formSubmit">Sign In</button>
+			</form>
+
+			<!--		<a href="https://recruitmentpro-app.chapterspot.com/prospects/view" target="_blank"><p>Visit Recruitment Pro!</p></a>-->
+
+		</div>
+
+
+		<?php getFooter(); ?>
+
+	</body>
+
+	<script>
+
+		function checkform() {
+			// get all the inputs within the submitted form
+			var inputs = document.getElementById("rushForm").getElementsByTagName('input');
+			for (var i = 0; i < inputs.length; i++) {
+				// only validate the inputs that have the required attribute
+				if(inputs[i].value == ""){
+					// found an empty field that is required
+					return false;
+				}
 			}
+			return true;
 		}
-		return true;
-	}
-	
-	$("#formSubmit").click(function() {
-		
-		if (checkform()) {
-		
-			if ($("#rushEmail").val().trim().indexOf("@bu.edu", this.length - "@bu.edu".length) == -1 || $("#rushEmail").val().length > 15) {
-				alert("Please use your BU email");
-				return;
+
+		$("#formSubmit").click(function() {
+
+			if (checkform()) {
+
+				if ($("#rushEmail").val().trim().indexOf("@bu.edu", this.length - "@bu.edu".length) == -1 || $("#rushEmail").val().length > 15) {
+					alert("Please use your BU email");
+					return;
+				}
+
+				$("#rushForm").submit();
+			} else {
+				alert("Please fill all required fields");
 			}
 
-			$("#rushForm").submit();
-		} else {
-			alert("Please fill all required fields");
-		}
-		
-		
-	});
-	
-	$('input').on('keydown', function(e) {
-		if (e.which == 13 || e.keyCode == 13) {
-			$("#formSubmit").click();
-		}
-	});
-	
-	var Rushes = new Array();
-	
-	<?php 
+
+		});
+
+		$('input').on('keydown', function(e) {
+			if (e.which == 13 || e.keyCode == 13) {
+				$("#formSubmit").click();
+			}
+		});
+
+		var Rushes = new Array();
+
+		<?php 
 		include("../manage_db/db_credentials.php");
 
 		// Create connection
@@ -179,7 +195,7 @@ switch ($event) {
 		// Check connection
 		if (mysqli_connect_errno())
 		{
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		}
 
 		//Get all entries to populate Javascript Object
@@ -196,25 +212,25 @@ switch ($event) {
 				. "RushInfo['Grade'] = '" . str_replace("'","",$row['Grade']) . "';\n"
 				. "Rushes['" . str_replace("'","",$row['Email']) . "'] = RushInfo;\n";
 		}
-	?>
-	
-	$('input#rushEmail').on('keyup', function(e) {
-//		alert($.inArray(Rushes, $(this).val() + "bu.edu"))
-		if ( $(this).val().indexOf("@") == $(this).val().length-1 && $.inArray(Rushes, $(this).val() + "bu.edu")) {
-			var rushesInfo = Rushes[$(this).val() + "bu.edu"];
-			$(this).val($(this).val() + "bu.edu");
-			$("input#rushFirstName").val(rushesInfo['FirstName']).parent().addClass("is-dirty");
-			$("input#rushLastName").val(rushesInfo['LastName']).parent().addClass("is-dirty");
-			$("input#rushPhone").val(rushesInfo['Phone']).parent().addClass("is-dirty");
-			$("input#rushMajors").val(rushesInfo['Majors']).parent().addClass("is-dirty");
-			$("input#rushSchool").val(rushesInfo['MajorSchools']).parent().addClass("is-dirty");
-			$("input#rushGrade").val(rushesInfo['Grade']).parent().addClass("is-dirty");
-		}
-		if ($(this).val().indexOf("@bu.edu") > 0) {
-			console.log($(this).val().indexOf("@bu.edu"));
-			console.log($(this).val());
-			$(this).val($(this).val().substring(0,$(this).val().indexOf("@bu.edu")+7));
-		}
-	});
-</script>
+		?>
+
+		$('input#rushEmail').on('keyup', function(e) {
+			//		alert($.inArray(Rushes, $(this).val() + "bu.edu"))
+			if ( $(this).val().indexOf("@") == $(this).val().length-1 && $.inArray(Rushes, $(this).val() + "bu.edu")) {
+				var rushesInfo = Rushes[$(this).val() + "bu.edu"];
+				$(this).val($(this).val() + "bu.edu");
+				$("input#rushFirstName").val(rushesInfo['FirstName']).parent().addClass("is-dirty");
+				$("input#rushLastName").val(rushesInfo['LastName']).parent().addClass("is-dirty");
+				$("input#rushPhone").val(rushesInfo['Phone']).parent().addClass("is-dirty");
+				$("input#rushMajors").val(rushesInfo['Majors']).parent().addClass("is-dirty");
+				$("input#rushSchool").val(rushesInfo['MajorSchools']).parent().addClass("is-dirty");
+				$("input#rushGrade").val(rushesInfo['Grade']).parent().addClass("is-dirty");
+			}
+			if ($(this).val().indexOf("@bu.edu") > 0) {
+				console.log($(this).val().indexOf("@bu.edu"));
+				console.log($(this).val());
+				$(this).val($(this).val().substring(0,$(this).val().indexOf("@bu.edu")+7));
+			}
+		});
+	</script>
 </html>
--- rush/rushes.php
+++ rush/rushes.php
@@ -1,6 +1,7 @@
 <?php
 
 include("/home/content/03/5577503/html/rush/password_protect_cookie.php");
+ini_set('display_errors', 1);
 
 ?>
 
@@ -8,54 +9,72 @@ include("/home/content/03/5577503/html/rush/password_protect_cookie.php");
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
+	<head>
 
-	<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.indigo-pink.min.css">
-	<title>Rushes | Alpha Kappa Psi Nu Chapter</title>
-	
-	<script src="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.js"></script>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	
-	<script src="../js/jquery.js"></script>
-	<script src="../js/jquery.color.js"></script>
-	<script src="../js/jquery.floatThead.min.js"></script>
-	
-	<style>
-		.mdl-title {
-			position: relative;
-			vertical-align: bottom;
-			text-overflow: ellipsis;
-			font-weight: 700;
-			letter-spacing: 0;
-			height: 48px;
-			font-size: 12px;
-			color: rgba(0,0,0,.54);
-			box-sizing: border-box;
-		}
-		table th {
-			background-color:#000033;
-		}
-		table th span {
-			color:white;
-		}
-	</style>
-	
-</head>
+		<link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.indigo-pink.min.css">
+		<title>Rushes | Alpha Kappa Psi Nu Chapter</title>
 
-<?php
+		<script src="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.min.js"></script>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
-include("../manage_db/db_credentials.php");
+		<script src="../js/jquery.js"></script>
+		<script src="../js/jquery.color.js"></script>
+		<script src="../js/jquery.floatThead.min.js"></script>
 
-// Create connection
-$con = new mysqli($hostname, $username, $password, $dbname);
+		<style>
+			.mdl-title {
+				position: relative;
+				vertical-align: bottom;
+				text-overflow: ellipsis;
+				font-weight: 700;
+				letter-spacing: 0;
+				height: 48px;
+				font-size: 12px;
+				color: rgba(0,0,0,.54);
+				box-sizing: border-box;
+			}
+			table th {
+				background-color:#000033;
+			}
+			table th span {
+				color:white;
+			}
+		</style>
 
-// Check connection
-if (mysqli_connect_errno())
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	</head>
 
-$result = mysqli_query($con,"SELECT COUNT(*), SUM(`InvitedToClosed`), SUM(`AppSubmitted`), SUM(`Info1`), SUM(`Info2`), SUM(`Fashion`), SUM(`Professional`), SUM(`Trivia`), SUM(`Mocktail`) FROM $rushTable");
-$row = mysqli_fetch_array($result);
-echo "
+	<?php
+
+	include("../manage_db/db_credentials.php");
+
+	// Create connection
+	$con = new mysqli($hostname, $username, $password, $dbname);
+
+	// Check connection
+	if (mysqli_connect_errno())
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
+	$fields = [
+		"Info1",
+		"Info2",
+		"Coffeehouse",
+		"Professional",
+		"Fashion",
+		"Interview",
+		"Mocktail"
+	];
+
+
+	$queryBody = "";
+
+	$i = 0;
+	for ($i = 0; $i < count($fields); $i++) {
+		$queryBody .= ", SUM(`" . $fields[$i] . "`)";
+	}
+
+	$result = mysqli_query($con,"SELECT COUNT(*), SUM(`InvitedToClosed`), SUM(`AppSubmitted`)" . $queryBody . " FROM $rushTable");
+	$row = mysqli_fetch_array($result);
+	echo "
 <table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>
 	<tr>
 		<td class='mdl-title'><div><span>Number of Rushes</span></div></td>
@@ -70,120 +89,109 @@ echo "
 		<td><div><span>".$row["SUM(`InvitedToClosed`)"]."</span></div></td>
 	</tr>
 </table>";
-echo "
-<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>
-	<tr>
-		<td class='mdl-title'><div><span>Info Session I</span></div></td>
-		<td><div><span>".$row["SUM(`Info1`)"]."</span></div></td>
-	</tr>
-	<tr>
-		<td class='mdl-title'><div><span>Info Session II</span><div></span></td>
-		<td><div><span>".$row["SUM(`Info2`)"]."</span></div></td>
-	</tr>
-	<tr>
-		<td class='mdl-title'><div><span>Fashion Night</span><div></span></td>
-		<td><div><span>".$row["SUM(`Fashion`)"]."</span></div></td>
-	</tr>
-	<tr>
-		<td class='mdl-title'><div><span>Professional Workshop</span></div></td>
-		<td><div><span>".$row["SUM(`Professional`)"]."</span></div></td>
-	</tr>
-	<tr>
-		<td class='mdl-title'><div><span>Trivia Night</span><div></span></td>
-		<td><div><span>".$row["SUM(`Trivia`)"]."</span></div></td>
-	</tr>
-	<tr>
-		<td class='mdl-title'><div><span>Mocktail Night</span><div></span></td>
-		<td><div><span>".$row["SUM(`Mocktail`)"]."</span></div></td>
-	</tr>
-</table>";
+	echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
 
-$result = mysqli_query($con,"SELECT `Channel`, COUNT(*) FROM $rushTable group by `Channel`");
-echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
-while($row = mysqli_fetch_array($result)) {
-	echo "<tr>
+	for ($i = 0; $i < count($fields); $i++) {
+		echo "<tr>
+			<td class='mdl-title'><div><span>$fields[$i]</span></div></td>
+			<td><div><span>".$row["SUM(`$fields[$i]`)"]."</span></div></td>
+			</tr>
+		";
+	}
+
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Info Session I</span></div></td>
+	//		<td><div><span>".$row["SUM(`Info1`)"]."</span></div></td>
+	//	</tr>
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Info Session II</span><div></span></td>
+	//		<td><div><span>".$row["SUM(`Info2`)"]."</span></div></td>
+	//	</tr>
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Fashion Night</span><div></span></td>
+	//		<td><div><span>".$row["SUM(`Fashion`)"]."</span></div></td>
+	//	</tr>
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Professional Workshop</span></div></td>
+	//		<td><div><span>".$row["SUM(`Professional`)"]."</span></div></td>
+	//	</tr>
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Trivia Night</span><div></span></td>
+	//		<td><div><span>".$row["SUM(`Trivia`)"]."</span></div></td>
+	//	</tr>
+	//	<tr>
+	//		<td class='mdl-title'><div><span>Mocktail Night</span><div></span></td>
+	//		<td><div><span>".$row["SUM(`Mocktail`)"]."</span></div></td>
+	//	</tr>
+	echo "</table>";
+
+	$result = mysqli_query($con,"SELECT `Channel`, COUNT(*) FROM $rushTable group by `Channel`");
+	echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
+	while($row = mysqli_fetch_array($result)) {
+		echo "<tr>
 		<td class='mdl-title'><div><span>".$row["Channel"]."</span></div></td>
 		<td><div><span>".$row["COUNT(*)"]."</span></div></td>
 	</tr>";
-}
-echo "</table>";
-	
-$result = mysqli_query($con,"SELECT `MajorSchools`, COUNT(*) FROM $rushTable group by `MajorSchools`");
-echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
-while($row = mysqli_fetch_array($result)) {
-	echo "<tr>
+	}
+	echo "</table>";
+
+	$result = mysqli_query($con,"SELECT `MajorSchools`, COUNT(*) FROM $rushTable group by `MajorSchools`");
+	echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
+	while($row = mysqli_fetch_array($result)) {
+		echo "<tr>
 		<td class='mdl-title'><div><span>".$row["MajorSchools"]."</span></div></td>
 		<td><div><span>".$row["COUNT(*)"]."</span></div></td>
 	</tr>";
-}
-echo "</table>";
-	
-$result = mysqli_query($con,"SELECT `Grade`, COUNT(*) FROM $rushTable group by `Grade`");
-echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
-while($row = mysqli_fetch_array($result)) {
-	echo "<tr>
+	}
+	echo "</table>";
+
+	$result = mysqli_query($con,"SELECT `Grade`, COUNT(*) FROM $rushTable group by `Grade`");
+	echo "<table class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px; display:inline-block'>";
+	while($row = mysqli_fetch_array($result)) {
+		echo "<tr>
 		<td class='mdl-title'><div><span>".$row["Grade"]."</span></div></td>
 		<td><div><span>".$row["COUNT(*)"]."</span></div></td>
 	</tr>";
-}
-echo "</table>";
+	}
+	echo "</table>";
 
-	
-//$result = mysqli_query($con,"SELECT * FROM $rushTable ORDER BY `InvitedToClosed` DESC, `AppSubmitted` DESC, `LastName`");
-$result = mysqli_query($con,"SELECT * FROM $rushTable ORDER BY `Sign Up Time`");
 
-echo "<table id='table' class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px'>
-<tr>
-<th><div><span>#</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Sign Up Time</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>First Name</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Last Name</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Email</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Phone</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Major(s)</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>School(s)</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Grade</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Channel</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Infosession 1</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Infosession 2</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Fashion Night</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Professional Workshop</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Submitted Application</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Invited to Closed Rush?</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Trivia Night</span></div></th>
-<th class='mdl-data-table__cell--non-numeric'><div><span>Mocktail Night</span></div></th>
-</tr>";
+	//$result = mysqli_query($con,"SELECT * FROM $rushTable ORDER BY `InvitedToClosed` DESC, `AppSubmitted` DESC, `LastName`");
+	$result = mysqli_query($con,"SELECT * FROM $rushTable ORDER BY `Sign Up Time`");
+	$fields = mysqli_num_fields($result);
 
-$x = 1;
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $x++ . "</td>";
-echo "<td>" . date('D M j, g:iA',strtotime($row['Sign Up Time'])) . "</td>";
-echo "<td>" . $row['FirstName'] . "</td>";
-echo "<td>" . $row['LastName'] . "</td>";
-echo "<td>" . $row['Email'] . "</td>";
-echo "<td>" . $row['Phone'] . "</td>";
-echo "<td>" . $row['Majors'] . "</td>";
-echo "<td>" . $row['MajorSchools'] . "</td>";
-echo "<td>" . $row['Grade'] . "</td>";
-echo "<td>" . $row['Channel'] . "</td>";
-echo "<td>" . ($row['Info1'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['Info2'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['Fashion'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['Professional'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['AppSubmitted'] ? "<a href=\"view_app.php?email=".$row['Email']."\">Yes</a>" : "No") . "</td>";
-echo "<td>" . ($row['InvitedToClosed'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['Trivia'] ? "Yes" : "No") . "</td>";
-echo "<td>" . ($row['Mocktail'] ? "Yes" : "No") . "</td>";
-echo "</tr>";
-}
-echo "</table>";
+	echo "<table id='table' class='mdl-data-table mdl-js-data-table mdl-shadow--2dp' style='margin:10px'>
+	<tr>
+	<th><div><span>#</span></div></th>";
 
-mysqli_close($con);
-?>
+	for ($i = 0; $i < $fields; $i++) {
+		echo "<th class='mdl-data-table__cell--non-numeric'><div><span>".mysqli_fetch_field_direct($result, $i)->name."</span></div></th>";
+	}
 
-<script>
-	var $table = $('#table');
-	$table.floatThead();
-</script>
+	echo "</tr>";
+
+	$x = 1;
+	while($row = mysqli_fetch_row($result))
+	{
+		echo "<tr>";
+		echo "<td>" . $x++ . "</td>";
+		for ($i = 0; $i < count($row); $i++) {
+
+			if(mysqli_fetch_field_direct($result, $i)->type == 1) {
+				echo "<td>" . ($row[$i] ? "Yes" : "No") . "</td>";
+			} else {
+				echo "<td>" . $row[$i] . "</td>";
+			}
+
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+
+	mysqli_close($con);
+	?>
+
+	<script>
+		var $table = $('#table');
+		$table.floatThead();
+	</script>
