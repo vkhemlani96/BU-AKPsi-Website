8bb20d25e8cdf96d67f871508243c349e27e18d2
vkhemlani96 <vkhemlani96@gmail.com>
2016-09-26 16:47:17 -0400

    DB Folder Cleanup

---
+++ db/credentials.php
@@ -0,0 +1,10 @@
+<?php
+//PHPMyAdmin URL - https://p3nlmysqladm002.secureserver.net/grid55/223
+$hostname="68.178.217.54";
+$username="AKPsiBrotherhood";
+$password="Brotherhood88!";
+$dbname="AKPsiBrotherhood";
+$rushTable="rushFall2016";
+$caseTable="caseSpring2016";
+$rushTableApps= $rushTable . "Apps";
+?>
--- db/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://p3nlmysqladm002.secureserver.net/grid55/223/index.php");
-die();
-?>
