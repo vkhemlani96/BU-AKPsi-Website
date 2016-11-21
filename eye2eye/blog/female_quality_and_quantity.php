98665b3ad3b3979f80f8fc3d52b5adbe4ba28bcf
vkhemlani96 <vkhemlani96@gmail.com>
2016-11-21 00:56:33 -0500

    E2E Blog Pretty Quotes Fix 3

--- eye2eye/blog/female_quality_and_quantity.php
+++ eye2eye/blog/female_quality_and_quantity.php
@@ -27,14 +27,14 @@
 		</div>
 		<div class="vertical_padding center blog_body">
 			<p>
-				There have been numerous studies done on gender equality across industries and countries; however, there aren’t many that focus on education, especially among universities. One study that stands out in this category is "The Glass Door Remains Closed: Another Look at Gender Inequality in Undergraduate Business Schools" by Laura Marini Davis and Victoria Geyfman<sup>1</sup>. The authors looked at female representation in undergraduate business schools by analyzing accredited U.S. business programs in the Association to Advance Collegiate Schools of Business (AACSB) from 2003 to 2011.
+				There have been numerous studies done on gender equality across industries and countries; however, there aren't many that focus on education, especially among universities. One study that stands out in this category is "The Glass Door Remains Closed: Another Look at Gender Inequality in Undergraduate Business Schools" by Laura Marini Davis and Victoria Geyfman<sup>1</sup>. The authors looked at female representation in undergraduate business schools by analyzing accredited U.S. business programs in the Association to Advance Collegiate Schools of Business (AACSB) from 2003 to 2011.
 				<br><br>
 				According to AACSB data, the number of male students enrolled increased approximately 15% from 2010 to 2013 while the number of female students fell slightly by 0.55%. The data also shows that female representation at accredited member institutions in AACSB in the United States declined 3.6% from 2003 to 2011. The authors also took a look at the degree attainment and noticed that even though the number of bachelor business degrees increased by 9% during 2003 through 2011, the number of degrees awarded to female students remained virtually the same, increasing only 0.21%. The authors concluded, female representation in undergraduate business schools measured by either enrollment or degrees awarded has declined in the last decade –remarkable findings in light of a nationally reported reversal in the gender gap.</p>
 			<br>
 			<img src="img/AACSB_Enrollment_and_Degrees_Conferred.jpg" width="700" />
 			<br>
 			<p>
-				In classroom settings, the study found that women were more vulnerable to interruptions and were generally uncomfortable competing against men. Therefore, they tend to under-participate.  On the other hand, the presence of female instructors had a positive effect on female students’ participation in classroom discussions. As a result, female-friendly institutional factors play a role in female representation at undergraduate business schools.
+				In classroom settings, the study found that women were more vulnerable to interruptions and were generally uncomfortable competing against men. Therefore, they tend to under-participate.  On the other hand, the presence of female instructors had a positive effect on female students' participation in classroom discussions. As a result, female-friendly institutional factors play a role in female representation at undergraduate business schools.
 				<br><br>
 				After examining a variety of variables that characterize school access, educational environment, and female  experience, the authors observed three important findings:
 			</p>
