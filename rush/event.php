9101e8e9d1f2279e967bbd9ce8119775723f4d15
vkhemlani96 <vkhemlani96@gmail.com>
2017-01-30 16:26:27 -0500

    Rush Autocomplete Enhancements

--- rush/event.php
+++ rush/event.php
@@ -227,7 +227,7 @@ if (array_key_exists($event, $eventList)) {
 	</body>
 
 	<script>
-		
+
 		function checkform() {
 			// get all the inputs within the submitted form
 			var inputs = document.getElementById("rushForm").getElementsByTagName('input');
@@ -264,7 +264,7 @@ if (array_key_exists($event, $eventList)) {
 			}
 		});
 
-		var Rushes = new Array();
+		var Rushes = {};
 		var RushInfo;
 
 		<?php 
@@ -283,43 +283,51 @@ if (array_key_exists($event, $eventList)) {
 
 		while($row = mysqli_fetch_array($result)) {
 
-			echo "RushInfo = new Array();"
+			echo "RushInfo = {};\n"
 				. "RushInfo['FirstName'] = '" . trim(str_replace("'","",$row['FirstName'])) . "';\n"
 				. "RushInfo['LastName'] = '" . trim(str_replace("'","",$row['LastName'])) . "';\n"
-				. "RushInfo['Email'] = '" . trim(str_replace("'","",$row['Email'])) . "';\n"
+				. "RushInfo['Email'] = '" . strtolower(trim(str_replace("'","",$row['Email']))) . "';\n"
 				. "RushInfo['Phone'] = '" . trim(str_replace("'","",$row['Phone'])) . "';\n"
 				. "RushInfo['Majors'] = '" . trim(str_replace("'","",$row['Majors'])) . "';\n"
 				. "RushInfo['MajorSchools'] = '" . trim(str_replace("'","",$row['MajorSchools'])) . "';\n"
 				. "RushInfo['Grade'] = '" . trim(str_replace("'","",$row['Grade'])) . "';\n"
-				. "Rushes['" . trim(str_replace("'","",$row['Email'])) . "'] = RushInfo;\n";
+				. "Rushes['" . trim(str_replace("'","",$row['Email'])) . "'] = RushInfo;"
+				. "console.log(\"" . trim(str_replace("'","",$row['Email'])) ."\");\n\n";
 		}
 		?>
 
+		var lastChar = -1;
+
 		$('input#rushEmail').on('keyup', function(e) {
-//			alert($.inArray(Rushes, $(this).val() + "bu.edu"))
-			if ( $(this).val().indexOf("@") == $(this).val().length-1 && $.inArray(Rushes, $(this).val() + "bu.edu")) {
-				var rushesInfo = Rushes[$(this).val() + "bu.edu"];
-				console.log(rushesInfo);
-				$(this).val($(this).val() + "bu.edu");
-				$("input#rushFirstName").val(rushesInfo['FirstName']).parent().addClass("is-dirty");
-				$("input#rushLastName").val(rushesInfo['LastName']).parent().addClass("is-dirty");
-				$("input#rushPhone").val(rushesInfo['Phone']).parent().addClass("is-dirty");
-				$("input#rushMajors").val(rushesInfo['Majors']).parent().addClass("is-dirty");
-				$("input#rushGrade").val(rushesInfo['Grade']).parent().addClass("is-dirty");
+			lastChar = e.which;
+			if (lastChar == 50) {
+				var email = $(this).val() + "bu.edu";
 				
-				$(".rush_grade .mdl-js-radio input[value='" + rushesInfo['Grade'] + "']").parent()[0].MaterialRadio.check();
+				$(this).val(email);
 				
-				var schools = rushesInfo['MajorSchools'].split(", ");
-				for (var i = 0; i < schools.length; i++) {
-					console.log($(".rush_schools input[value='" + schools[i] + "']"));
-					$(".rush_schools .mdl-js-checkbox input[value='" + schools[i] + "']").parent()[0].MaterialCheckbox.check();
+				if ($(this).val().toLowerCase() in Rushes) {
+
+					var rushesInfo = Rushes[email];
+					$("input#rushFirstName").val(rushesInfo['FirstName']).parent().addClass("is-dirty");
+					$("input#rushLastName").val(rushesInfo['LastName']).parent().addClass("is-dirty");
+					$("input#rushPhone").val(rushesInfo['Phone']).parent().addClass("is-dirty");
+					$("input#rushMajors").val(rushesInfo['Majors']).parent().addClass("is-dirty");
+					$("input#rushGrade").val(rushesInfo['Grade']).parent().addClass("is-dirty");
+
+					$(".rush_grade .mdl-js-radio input[value='" + rushesInfo['Grade'] + "']").parent()[0].MaterialRadio.check();
+
+					var schools = rushesInfo['MajorSchools'].split(", ");
+					for (var i = 0; i < schools.length; i++) {
+						console.log($(".rush_schools input[value='" + schools[i] + "']"));
+						$(".rush_schools .mdl-js-checkbox input[value='" + schools[i] + "']").parent()[0].MaterialCheckbox.check();
+					}
+				} else {
+					$("input#rushFirstName").focus();
 				}
+
 			}
-			if ($(this).val().indexOf("@bu.edu") > 0) {
-				console.log($(this).val().indexOf("@bu.edu"));
-				console.log($(this).val());
-				$(this).val($(this).val().substring(0,$(this).val().indexOf("@bu.edu")+7));
-			}
+			
+			
 		});
 	</script>
 </html>
