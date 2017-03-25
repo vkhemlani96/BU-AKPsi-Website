55594abfdc79cac9034cb8e96ec7d3cdbbe0c00e
vkhemlani96 <vkhemlani96@gmail.com>
2017-03-25 10:05:11 -0400

    Update Voting Page

--- vote.php
+++ vote.php
@@ -1,3 +1,3 @@
 <?php
-header("Location: https://docs.google.com/a/bu.edu/forms/d/e/1FAIpQLSeSPIyCmJOupaHk_noRSFXyw379sk_57BxqVJ5d9tfrJdEVjg/viewform") ;
+header("Location: https://docs.google.com/forms/d/e/1FAIpQLScQvFKGvH-kxvlqKR_Xbbuju053BIwQImzj_1zem_ffwdHD7Q/viewform?usp=sf_link") ;
 ?>
